019a4f937e81240f94603ec860dd78deb50f9850
jaeyoung <jalpr16@gmail.com>
2019-01-04 00:46:14 +0900

    Made it work repeatedly

--- main.py
+++ main.py
@@ -3,12 +3,16 @@
 
 if __name__ == '__main__':
     print("Hello World!")
-    try:
-        print(list(map(int, input("Enter a list of numbers: ").split())))
-    except ValueError:
-        print("Told you to enter a list of numbers!")
-    else:
-        print("Thank you!")
+    while(True):
+        try:
+            print(list(map(int, input("Enter a list of numbers: ").split())))
+        except ValueError:
+            print("Told you to enter a list of numbers!")
+        else:
+            print("Thank you!")
+        finally:
+            print("This sentence will be shown regardless!")
+
 
 
 
