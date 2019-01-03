b747bf6b8be8a907c3b700f31bee7c9e4b2ee6c1
jaeyoung <jalpr16@gmail.com>
2019-01-04 00:40:03 +0900

    added a feature to ask for user to enter a list of numbers

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+
 # some useless comment
 
 if __name__ == '__main__':
     print("Hello World!")
+    try:
+        print(list(map(int, input("Enter a list of numbers: ").split())))
+    except ValueError:
+        print("Told you to enter a list of numbers!")
+    else:
+        print("Thank you!")
+
+
+
